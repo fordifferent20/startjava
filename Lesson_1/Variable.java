b109b253181c0c3e5c5a045162ef188759d13dfa
Тарасов Алексей Александрович <A.Tarasov@inet.vtb>
2020-01-16 09:15:14 +0300

    HW_1

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -2,15 +2,17 @@ public class Variable {
     public static void main(String[] args)
     {
     // Обявление в методе переменных всех существующих в Java примитивных типов данных. Каждой переменной присвоено значение.
-    char c = 'I'; // Символ
-    int i = 5; // Intel i5
-    byte b = 4; // 4 ядра
-    short s = 4590; // серия 4590
-    long l = 4600; // Графика
-    float f = 3.30f; // Частота GHz
-    double d = 3.3030d; // Двойная точность частоты GHz
-    boolean bl = true;
-        System.out.println("Процессор " + b + " ядра " + "Intel " + c + i +"-" + s + " CPU " + f + " GHz" + " (" + d + " GHz" + ")");
+        char c = 'I'; 
+        int i = 5; 
+        byte b = 4; 
+        short s = 4590; 
+        long l = 4600; 
+        float f = 3.30f; 
+        double d = 3.3030d; 
+        boolean bl = true;
+
+    // Вывод    
+        System.out.println("CPU " + b + " core " + "Intel " + c + i +"-" + s + " CPU " + f + " GHz" + " (" + d + " GHz" + ")");
         System.out.println("HD Graphics " + l);
         System.out.println(" ");
         System.out.println(bl);
